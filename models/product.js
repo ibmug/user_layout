a3ab11bdf1b816adf3ad3eab777cf3537e7b8aa7
Ibmug <rosv58@gmail.com>
2021-01-26 19:47:39 -0600

    Adding many to many relationship in listaYProduct

--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes){
-    var Product = sequelize.define("Product", {
+    const Product = sequelize.define("Product", {
         //Giving the Product model a name of type stirng.
         //ID // Nombre //Categoria // Subcatogery //Marca //SKU
         
@@ -12,8 +12,10 @@ module.exports = function(sequelize, DataTypes){
 
         Product.associate = function(models){   
             //Product.belongsTo(models.GroceryList); //Adds id to Grocery List?
-            Product.hasMany(models.GroceryListProduct);
-        };
+            Product.belongsToMany(models.GroceryList, {through:models.GroceryListProduct});
+
+            
+        }
 
         return Product;
 
